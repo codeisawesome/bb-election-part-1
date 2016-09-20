7c8a5c87614015dea50aef4f68cccb4ee8bcbc8e
Rui Dong <rui.dong.jh@gmail.com>
2016-09-20 14:44:48 -0400

    parse name and votes countes for each person.

--- election.js
+++ election.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
 
-  // Imagination!
+    $.ajax({
+      url: "https://bb-election-api.herokuapp.com/",
+      method: "GET",
+      data: {},
+      dataType: "json"
+    }).done(function (responseData){
+      var people = responseData.candidates;
+      $('<ul>').appendTo('body');
+      people.forEach(function(person){
+      $('<li>').append('name: ' + person.name + ' votes: ' + person.votes).appendTo('body ');
+      })
+    })
 
 });
